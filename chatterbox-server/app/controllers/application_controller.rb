5ae8e55cedf4e5f2e508aaab0984eb8e2ad8e49c
Mwanasiti <ali.mariam.mwanasiti@gmail.com>
2022-09-13 10:22:28 +0300

    milestone:

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -2,5 +2,31 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add routes
+  get '/messages' do
+    everything = Message.all.order(created_at: :asc)
+    everything.to_json
+  end
+
+  post '/messages' do
+    send_it = Message.create(
+      body: params[:body],
+      username: params[:username]
+    )
+    send_it.to_json
+  end
+
+  patch '/messages/:id' do
+    fix_it = Message.find(params[:id])
+    fix_it.update(
+      body: params[:body]
+    )
+    fix_it.to_json
+  end
+
+  delete '/messages/:id' do
+    yeet_it = Message.find(params[:id])
+    yeet_it.destroy
+    yeet_it.to_json
+  end
   
 end
